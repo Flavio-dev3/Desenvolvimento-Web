18adfeed7d0b328b69f8fba4e5cffd2033d5267e
Flavio-dev3 <flavioemailpessoal3355@gmail.com>
2025-02-24 21:41:09 -0300

    Add files via upload
    aula desenvolvimento web

---
+++ deselvo.js
@@ -0,0 +1,53 @@
+/*ex:2
+var fibonacci = [0,1]
+var x = 10
+var auxi
+for(var c = 0;c < x -2; c++){
+    auxi = fibonacci [c] + fibonacci[c + 1]
+    fibonacci[c + 2] = auxi
+
+}
+alert(fibonacci);*/
+var marco = prompt("Digite algo: ")
+if(marco <= 0);
+
+
+alert(marco);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
