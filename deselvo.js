1662bce71fd91d0b52d3fb28699ce3a3d22811d8
Flavio-dev3 <flavioemailpessoal3355@gmail.com>
2025-02-25 20:33:29 -0300

    Add files via upload

--- deselvo.js
+++ deselvo.js
@@ -8,19 +8,27 @@ for(var c = 0;c < x -2; c++){
 
 }
 alert(fibonacci);*/
-var marco = prompt("Digite algo: ")
-if(marco <= 0);
+//ex:1
+var num1 = prompt("Digite um número  \n", "Digite aqui: ")
 
-
-alert(marco);
-
-
-
-
-
-
-
-
+if(num1 % 2 == 0){
+    alert(`O número:${num1} é par`)
+}
+else{
+    alert(`O número:${num1} é impar`)
+}
+var cont = 0
+for(c = 1; c <= num1;c++){
+    if(num1 % c == 0){
+        cont++
+    }
+}
+if(cont == 2){
+    alert(`O número: ${num1} é primo`)
+}
+else{
+    alert(`O número ${num1} não é primo`)
+}
 
 
 
