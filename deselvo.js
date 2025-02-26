c73f90080d1484a4bb90e663f85fa641cf519591
Flavio-dev3 <flavioemailpessoal3355@gmail.com>
2025-02-25 21:42:41 -0300

    Add files via upload
    25/02

--- deselvo.js
+++ deselvo.js
@@ -1,15 +1,13 @@
-/*ex:2
-var fibonacci = [0,1]
-var x = 10
-var auxi
-for(var c = 0;c < x -2; c++){
-    auxi = fibonacci [c] + fibonacci[c + 1]
-    fibonacci[c + 2] = auxi
+ex:2
+/*var fibonacci = [0,1]
+var x = prompt("Digite um número: ")
+for(c = 0;c < x -2; c++){
+    fibonacci[c + 2] = fibonacci [c] + fibonacci[c + 1]
 
 }
 alert(fibonacci);*/
-//ex:1
-var num1 = prompt("Digite um número  \n", "Digite aqui: ")
+ex:1
+/*var num1 = prompt("Digite um número  \n", "Digite aqui: ")
 
 if(num1 % 2 == 0){
     alert(`O número:${num1} é par`)
@@ -28,10 +26,14 @@ if(cont == 2){
 }
 else{
     alert(`O número ${num1} não é primo`)
+}*/
+ex:3
+var lista = []
+var xc = prompt("Digite a quantidade de números na lista")
+for(c = 0;c <= xc;c++){
+    bc = prompt("Digite um número:")
+    lista[c]
 }
-
-
-
 
 
 
