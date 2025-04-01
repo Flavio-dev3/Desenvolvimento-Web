ed1353128c5e599b3cc0817e826113defd7e3d9f
Flavio-dev3 <flavioemailpessoal3355@gmail.com>
2025-03-31 21:45:52 -0300

    Add files via upload

--- cookie.js
+++ cookie.js
@@ -41,7 +41,8 @@ verificarCookie();
 //A aplicação deve também ter uma opção sair, que obriga o usuário a logar novamente na pró-
 //xima vez que utilizar a aplicação.
 
-
+//ATIVIDADE
+/*
 function criarCookie(nome, valor, expira){
     var dtExpira = "expires="+expira;
     document.cookie = nome + "=" + valor + "; " + dtExpira;
@@ -123,10 +124,77 @@ while(menu != 5){
     }
 
 }
+*/
+// TABUADA COM COOKIE
 
- 
+function criarCookie(nome, valor, expira){
+    var dtExpira = "expires="+expira;
+    document.cookie = nome + "=" + valor + "; " + dtExpira;
+    //cookie final = nomeUsuario=flavio; expires= Tue, 01 Jan 2115 12:00:00 UTC 
+}
 
+function lerCookie(nome){
+    var vnome = nome + "=";
+    //vnome -> nomeUsuario=
+    var ca = document.cookie.split(';');
+    //ca -> nomeUsuario=nome
+    for(var i=0; i<ca.length; i++){
+        var c = ca[i];
+        while(c.charAt(0)==' '){
+            c = c.substring(1);
+        }
+        if(c.indexOf(vnome) == 0)
 
+            return c.substring(vnome.length,c.length);
+    }
+    return "";
+}
+function verificarCookie(){
+    username=lerCookie("Tabuada");
+    if(username != ""){
+        tabuada();
+    }else{
+        username =  username =  prompt("Digite um número para ver a sua tabuada de 1 a 10:");
+        if (username != "" && username != null){
+            tabuada();
+        }
+    }
+}
+var alte;
+function alterartabuada(){
+    alte =   prompt("Digite outro número para ver a sua tabuada de 1 a 10:");
+    for(var i=1;i<=10;i++){
+        var nm = alte * i;
+        var result = alert(`${alte} X ${i} = ${nm}`);
+        criarCookie("Tabuada", alte, " Tue, 01 Jan 2115 12:00:00 UTC ");
+    }
+}
+function tabuada(){
+    for(var i=1;i<=10;i++){
+        var nm = username * i;
+        var result = alert(`${username} X ${i} = ${nm}`);
+        criarCookie("Tabuada", username, " Tue, 01 Jan 2115 12:00:00 UTC ");
+    }
+}
+function excluir(){
+    criarCookie("Tabuada", username, " Tue, 01 Jan 1999 12:00:00 UTC ");
+    criarCookie("Tabuada", alte, " Tue, 01 Jan 1999 12:00:00 UTC ");
+}
+var username;
+verificarCookie();
+var menu
+while(menu != 4){
+    menu = prompt("Digite 1 para Alterar tabuada\n Digite 2 para ver a tabuada anterior\nDigite 3 para excluir suas tabuadas\nDigite 4 para atualizar");
+    if(menu == 1){
+        alterartabuada();
+    }
+    if(menu == 2){
+        verificarCookie();
+    }
+    if(menu == 3){
+        excluir();
+    }
+}
 
 
 
--- programas.js
+++ programas.js
@@ -12,6 +12,7 @@ function tabuada(){
 tabuada();
 */
 
+/*
 //Desenvolvimento de uma função chamada média e calcule o valor da média aritmética.
 var input = prompt("Digite os números separada por virgulas");
 var lista = input.split(",");
@@ -27,9 +28,48 @@ function media(){
 }
 // Preciso chamar a função fora dela para ela ser executada.
 media();
+*/
+function criarCookie(nome, valor, expira){
+    var dtExpira = "expires="+expira;
+    document.cookie = nome + "=" + valor + "; " + dtExpira;
+    //cookie final = nomeUsuario=flavio; expires= Tue, 01 Jan 2115 12:00:00 UTC 
+}
 
+function lerCookie(nome){
+    var vnome = nome + "=";
+    //vnome -> nomeUsuario=
+    var ca = document.cookie.split(';');
+    //ca -> nomeUsuario=nome
+    for(var i=0; i<ca.length; i++){
+        var c = ca[i];
+        while(c.charAt(0)==' '){
+            c = c.substring(1);
+        }
+        if(c.indexOf(vnome) == 0)
 
-
+            return c.substring(vnome.length,c.length);
+    }
+    return "";
+}
+function verificarCookie(){
+    username=lerCookie("Tabuada");
+    if(username != ""){
+        tabuada();
+    }else{
+        username =  username =  prompt("Digite um número para ver a sua tabuada de 1 a 10:");
+        if (username != "" && username != null){
+            tabuada();
+        }
+    }
+}
+function tabuada(){
+    for(var i=1;i<=10;i++){
+        var nm = username * i;
+        var result = alert(`${username} X ${i} = ${nm}`);
+        criarCookie("Tabuada", username, " Tue, 01 Jan 2115 12:00:00 UTC ");
+    }
+}
+verificarCookie();
 
 
 
