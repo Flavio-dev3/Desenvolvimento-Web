769046c1a4a1b7c99c7afccde433559bee6d908f
Flavio-dev3 <flavioemailpessoal3355@gmail.com>
2025-04-04 21:49:36 -0300

    Add files via upload

---
+++ manc.js
@@ -0,0 +1,30 @@
+const entrada = require('prompt-sync')({sigint:true});
+/*num1 = parseInt(entrada('Digite um número:'));
+num2 = parseInt(entrada('Digite outro número:'));
+sm = num1 + num2;
+console.log(sm);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
